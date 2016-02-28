278b2701a11d0b4643b2e8d954857e9b35fccc79
Filip Smets <alemaaltevinden@gmail.com>
2016-02-28 13:58:11 +0100

    Remove sendmail cause it doesn't work

--- js/main.js
+++ js/main.js
@@ -203,7 +203,22 @@ jQuery(function($) {'use strict';
 				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
 			}
 		}).done(function(data){
-			form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+			form_status.html('<p class="text-success">Bericht ontvangen! We zullen zo snel mogelijk antwoorden.</p>').delay(3000).fadeOut();
+		});
+	});
+
+	// Contact form
+	var form = $('#main-contact-form2');
+	form.submit(function(event){
+		event.preventDefault();
+		var form_status = $('<div class="form_status"></div>');
+		$.ajax({
+			url: $(this).attr('action'),
+			beforeSend: function(){
+				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+			}
+		}).done(function(data){
+			form_status.html('<p class="text-success">Thank you for contacting us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
 		});
 	});
 
